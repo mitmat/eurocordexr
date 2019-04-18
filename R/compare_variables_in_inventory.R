d6d552bad35824229cbe6394b73cd52d25d8446b
Michael Matiu <michael.matiu@eurac.edu>
2019-04-18 13:28:32 +0000

    added get and compare inventory functions

---
+++ R/compare_variables_in_inventory.R
@@ -0,0 +1,44 @@
+
+#' Compare an EURO-CORDEX inventory for different variables
+#'
+#' Casts the result from \code{\link{get_inventory}} for different variables in
+#' order to compare completeness of the inventory. Adds columns for checking
+#' equality of years and number of files.
+#'
+#' @param data_inventory A data.table as resulting from
+#'   \code{\link{get_inventory}}.
+#' @param vars Character vector of variables to compare.
+#'
+#' @return The casted data.table with boolean columns if all years and number of
+#'   files are equal for all variables.
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'
+#' path <- "/mnt/CEPH_BASEDATA/METEO/SCENARIO"
+#' dat <- get_inventory(path)
+#' dat_compare <- compare_variables_in_inventory(dat, c("tas","rsds","pr"))
+#' }
+compare_variables_in_inventory <- function(data_inventory, vars){
+
+  dat <- copy(data_inventory)
+
+  if("list_files" %in% names(dat)) {
+    dat[, list_files := NULL]
+  }
+
+  dat[variable %in% vars] %>%
+    dcast(... ~ variable, value.var = c("nn_files", "years_total")) -> dat_compare
+
+  dat_compare[,
+              all_nn_files_equal := apply(as.matrix(.SD), 1, function(x) length(unique(x)) == 1),
+              .SDcols = patterns("nn_files")]
+  dat_compare[,
+              all_years_total_equal := apply(as.matrix(.SD), 1, function(x) length(unique(x)) == 1),
+              .SDcols = patterns("years_total")]
+
+  dat_compare
+
+}
---
+++ R/get_inventory.R
@@ -0,0 +1,68 @@
+#' Get inventory from path containing EURO-CORDEX .nc files
+#'
+#' Returns a data.table with information by splitting the netcdf files into
+#' their components (GCM, RCM, variable, experiment, ...) and aggregates over
+#' years.
+#'
+#' @param path Path that will be searched recursively for .nc files.
+#' @param add_files Boolean, if \code{TRUE}, will add a column containing lists
+#'   of associated files with their full paths (useful e.g. for further
+#'   processing).
+#'
+#' @return A data.table with the inventory information.
+#'
+#' @seealso \code{\link{compare_variables_in_inventory}} for further comparing the results.
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'
+#' path <- "/mnt/CEPH_BASEDATA/METEO/SCENARIO"
+#' dat <- get_inventory(path)
+#' print(dat)
+#'
+#' # the same, but with files (does not print nicely)
+#' dat_file <- get_inventory(path, add_files = T)
+#' print(dat_file)
+#' }
+get_inventory <- function(path,
+                          add_files = F){
+
+  all_files_fullpath <- list.files(path,
+                                   pattern = ".nc$",
+                                   recursive = T,
+                                   full.names = T)
+
+  all_files_base <- basename(all_files_fullpath)
+
+  # create info data
+  data.table(fn = all_files_base) %>%
+    .[, tstrsplit(fn, "_")] %>%
+    .[, V9 := gsub(".nc", "", V9)] -> dat_info
+
+  setnames(dat_info, c("variable", "domain", "gcm", "experiment","ensemble",
+                       "institute_rcm", "downscale_realisation", "timefreq", "years"))
+
+  dat_info[, file_fullpath := all_files_fullpath]
+
+
+  # get unique models
+  dat_info_summary <- dat_info[,
+                               .(nn_files = .N,
+                                 list_files = list(file_fullpath),
+                                 years_total = paste0(.SD[1, years %>% strsplit("-") %>% .[[1]] %>% .[1]],
+                                                      "-",
+                                                      .SD[.N, years %>% strsplit("-") %>% .[[1]] %>% .[2]])),
+                               keyby = .(variable, domain, gcm, institute_rcm, experiment,
+                                         ensemble, downscale_realisation, timefreq)]
+
+  # remove files if not requested
+  if(!add_files) dat_info_summary[, list_files := NULL]
+
+
+  return(dat_info_summary)
+}
+
+
+
